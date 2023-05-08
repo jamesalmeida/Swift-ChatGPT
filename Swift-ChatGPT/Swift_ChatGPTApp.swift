24ff8b0b7f593ddaea387147362771a9c5e544ef
James Almeida <jimmyplaysdrums@gmail.com>
2023-05-08 15:55:37 -0700

    Initial Commit

---
+++ Swift-ChatGPT/Swift_ChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_ChatGPTApp.swift
+//  Swift-ChatGPT
+//
+//  Created by James on 5/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Swift_ChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
